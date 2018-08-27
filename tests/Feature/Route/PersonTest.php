40dae6be39df925fe4dda0686f8eb31ae02850da
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-27 23:03:37 +0900

    PersonTestを作成
    * PersonTestを作成

---
+++ tests/Feature/Route/PersonTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature\Route;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PersonTest extends TestCase
+{
+    public function testPersonList()
+    {
+        $response = $this->json('GET', 'api/person');
+        $response->assertStatus(200);
+    }
+
+    public function testPersonStore()
+    {
+        $response = $this->json(
+            'POST',
+            'api/person',
+            [
+                'name' => 'Johnny',
+                'weight' => '60',
+                'height' => '1.72',
+            ]
+        );
+    }
+
+    public function testPersonView()
+    {
+        $response = $this->json('GET', 'api/person/1');
+        $response->assertStatus(200);
+    }
+
+    public function testPersonUpdate()
+    {
+        $response = $this->json('POST', 'api/person/1', ['name' => 'Johnson']);
+        $response->assertStatus(200);
+    }
+
+    public function testPersonDelete()
+    {
+        $response = $this->json('DELETE', 'api/person/1');
+        $response->assertStatus(200);
+    }
+
+}
