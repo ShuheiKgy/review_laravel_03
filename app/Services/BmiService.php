cd6c881e21cf64e52778d10551185426cb56e3d0
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-29 22:11:09 +0900

    BmiServiceクラス追加
    * BmiServiceクラス追加

---
+++ app/Services/BmiService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services;
+
+
+class BmiService
+{
+    public static function getBmi(Person $person)
+    {
+
+    }
+
+    private static function calcBmi(float $height, float $weight)
+    {
+
+    }
+}
