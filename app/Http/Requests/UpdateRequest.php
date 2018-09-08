72d76f388fb6671edfc3ffb23ed110843829fa8c
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-09-08 22:35:30 +0900

    UpdateRequestを実装
    * UpdateRequestを実装

--- app/Http/Requests/UpdateRequest.php
+++ app/Http/Requests/UpdateRequest.php
@@ -24,7 +24,9 @@ class UpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|max:255',
+            'weight' => 'required|min:0.1',
+            'height' => 'required|min:0.1',
         ];
     }
 }
